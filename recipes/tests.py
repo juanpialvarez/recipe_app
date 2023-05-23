5fa691bc4ebd7121fedb087f9afbc709abd284ee
juanpialvarez <juanpabloalvarezv@gmail.com>
2023-05-23 18:39:44 +0200

    testing

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Recipe
 
-# Create your tests here.
+class RecipeTest(TestCase):
+    def setUpTestData():
+       Recipe.objects.create(name='Salad', ingredients='Lettuce, tomato, dressing, onion, crackers', cooking_time=5, difficulty='easy')
+    def test_recipe_name(self):
+       book = Recipe.objects.get(id=1)
+       field_label = book._meta.get_field('name').verbose_name
+       self.assertEqual(field_label, 'name')
+    def test_ingredients_max_length(self):
+       book = Recipe.objects.get(id=1)
+       max_length = book._meta.get_field('ingredients').max_length
+       self.assertEqual(max_length, 255)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class UserTest(TestCase):
+    def setUpTestData():
+        User.objects.create(name="John Doe")
+
+    def test_user_name(self):
+        user = User.objects.get(id=1)
+        field_label = user._meta.get_field("name").verbose_name
+        self.assertEqual(field_label, "name")
