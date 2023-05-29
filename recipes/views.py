e7e27d3131bc96ba0988db3764e886a3638f8cb6
juanpialvarez <juanpabloalvarezv@gmail.com>
2023-05-29 15:45:00 +0200

    add auth

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = "recipes/main.html"
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = "recipes/detail.html"
 
