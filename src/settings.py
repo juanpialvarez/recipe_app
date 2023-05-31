7f35755c099a3d85e4cbd4f4f734129eb4eea1ee
juanpialvarez <juanpabloalvarezv@gmail.com>
2023-05-31 17:30:36 +0200

    Update ALLOWED_HOSTS with site and development server URL

--- src/settings.py
+++ src/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://fathomless-badlands-39710.herokuapp.com/"]
 
 
 # Application definition
